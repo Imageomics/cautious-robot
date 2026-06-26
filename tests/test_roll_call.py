a9e015b35da4421d6a87ca3df44b8fd2ad3cacf6
Ishika Aahna <123102828+codedbyishika@users.noreply.github.com>
2026-06-26 19:06:57 -0400

    Update imports

--- tests/test_roll_call.py
+++ tests/test_roll_call.py
@@ -1,7 +1,5 @@
-import os
-import sys
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 import pandas as pd
 
 from cautiousrobot.roll_call import RollCall
