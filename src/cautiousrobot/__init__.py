1b30b8d777106ecff2ccf4da69c3c9d82b1a2bf2
Ishika Aahna <123102828+codedbyishika@users.noreply.github.com>
2026-08-14 15:44:07 -0400

    Ruff Fix

--- src/cautiousrobot/__init__.py
+++ src/cautiousrobot/__init__.py
@@ -2,6 +2,4 @@ from cautiousrobot.buddy_check import BuddyCheck
 from cautiousrobot.download import download_images
 from cautiousrobot.utils import downsample_and_save_image
 
-
-
 __all__ = ["BuddyCheck", "download_images", "downsample_and_save_image"]
